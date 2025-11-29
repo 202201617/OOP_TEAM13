e6512938b21691dd0a78438ea6cea21f3e276e0b
202201617 <113917440+202201617@users.noreply.github.com>
2025-11-29 20:24:34 +0900

    Update object.py

--- object.py
+++ object.py
@@ -58,7 +58,7 @@ class CoffeeCup(Object):
         super().__init__(x, y, "image/coffee.png")
 
 COMMON_ITEMS = [Book, Energy, Soju, Nut, CoffeeCup]
-COMMON_WEIGHTS = [45, 10, 25, 10, 10]
+COMMON_WEIGHTS = [40, 10, 30, 10, 10]
 
 '''
 추가로 명수당에서는 장애물 없이 아이템만 등장하고 점수가 변동되는 데
@@ -69,7 +69,7 @@ def create_random_items(seconds, speed, screen_width, screen_height, stage_name,
     items = []
 
     #30초동안 나오게 되어있음
-    total_distance = (seconds + 2) * 30 * speed
+    total_distance = (seconds + 2) * 20 * speed
     current_x = screen_width + 100
 
     current_pool = COMMON_ITEMS[:]
@@ -91,6 +91,10 @@ def create_random_items(seconds, speed, screen_width, screen_height, stage_name,
         bonus_item_class = O_stu
         current_pool.append(bonus_item_class)
         curernt_weights.append(5)
+
+    elif stage_name == "bonus":
+        current_pool = [BonusBook, Energy]
+        curernt_weights = [80, 20]
 
     is_spawned = False
 
